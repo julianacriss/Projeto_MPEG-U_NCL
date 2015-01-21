677ef0b2b4b7e6e72f5b73e20ed5bfb2a7e924ef
Juliana Cristina de Andrade <julianacristina.ti@gmail.com>
2015-01-21 07:12:46 -0200

    Versão 3.1
    Pequenas modificações no código java que não alteram as funcionalidades
    do programa.

--- src/client/Main.java
+++ src/client/Main.java
@@ -2,10 +2,8 @@ package client;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.util.*;
